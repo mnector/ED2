e4ce91372401ca7aaad9b9c809940d46eccaf3c0
mnector <mnector@gmail.com>
2026-09-13 04:31:34 -0400

    Fix cargo features for winapi

---
+++ DynamicPacingASI/target/release/build/winapi-35629cdb58ae9bed/build_script_build-35629cdb58ae9bed.d
@@ -0,0 +1,5 @@
+C:\Users\mnect\.hermes\tools\ED2\DynamicPacingASI\target\release\build\winapi-35629cdb58ae9bed\build_script_build-35629cdb58ae9bed.d: C:\Users\mnect\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winapi-0.3.9\build.rs
+
+C:\Users\mnect\.hermes\tools\ED2\DynamicPacingASI\target\release\build\winapi-35629cdb58ae9bed\build_script_build-35629cdb58ae9bed.exe: C:\Users\mnect\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winapi-0.3.9\build.rs
+
+C:\Users\mnect\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winapi-0.3.9\build.rs:
